0311626346d43a8115370fc0d6c4c4033637e866
Steven Hogenson <33229415+SZ-Vapor@users.noreply.github.com>
2017-12-06 19:50:04 -0500

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,7 +1,5 @@
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -15,10 +13,9 @@ import java.util.Collections;
 public class Graph {
 
     static int size;
-    public static int costMatrix[][];
-    public int adJ[][];
+    static int costMatrix[][];
+    int adJ[][];
     boolean[] visited;
-    static ArrayList<Integer> nodes = new ArrayList<Integer>();
 
     public Graph(int size){
         this.size=size;
@@ -27,7 +24,7 @@ public class Graph {
         for(int i=0;i<size;i++)
             for(int j=0;j<size;j++)
             {
-                costMatrix[i][j]=0;
+                //costMatrix[i][j]=0;
                 adJ[i][j]=0;
             }
         
@@ -44,7 +41,13 @@ public class Graph {
     }
     
     public boolean isConnected(int x, int y){
-        return costMatrix[x][y]!=0||costMatrix[y][x]!=0;
+        return adJ[x][y]==1||adJ[y][x]==1;
+    }
+    public int[][] getAdjacencyMatrix(){
+        return adJ;
+    }
+    public int getCostMatrixValueAt(int x,int y){
+        return costMatrix[x][y];
     }
     
     public void displayMatrix(){
@@ -55,9 +58,22 @@ public class Graph {
         System.out.println();
         
         for(int i=0;i<size;i++){
-            System.out.print(String.format("%4s", i+"| "));
+            //System.out.print(String.format("%4s", i+"| "));
             for(int j=0;j<size;j++)
                 System.out.print(String.format("%4s", adJ[i][j]));
+            System.out.println();
+        }
+    }
+    
+    public void displayCostMatrix(){
+        System.out.print("COST MATRIX: ");
+        
+        System.out.println();
+        
+        for(int i=0;i<size;i++){
+            //System.out.print(String.format("%4s", i+"| "));
+            for(int j=0;j<size;j++)
+                System.out.print(String.format("%4s", costMatrix[i][j]));
             System.out.println();
         }
     }
